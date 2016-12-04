6bb345554389bac4de4ddf5beb253f8f7fc1122c
Austin Lauritsen <austin@veryaustin.com>
2016-12-04 13:31:58 -0600

    Create header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {PropTypes} from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <p>Header goes here</p>
+        <Header />
         {this.props.children}
       </div>
     );
@@ -13,6 +14,6 @@ class App extends React.Component {
 
 App.propTypes= {
   children: PropTypes.object.isRequired
-}
+};
 
-export default App:
+export default App;
