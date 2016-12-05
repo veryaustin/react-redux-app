b4ae46b11ab3e28d4eb406698837cda086802ad9
Austin Lauritsen <austin@veryaustin.com>
2016-12-05 16:50:29 -0600

    Testing mapStateToProps

---
+++ src/selectors/selectors.spec.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import {authorsFormattedForDropDown} from './selectors';
+
+describe('Author Selectors', () => {
+  describe('authorsFormattedForDropdown', () => {
+    it('should return author data formatted for use in a dropdown', () => {
+      const authors = [
+        {id: 'austin-smith', firstName: 'Austin', lastName: 'Smith'},
+        {id: 'scott-allen', firstName: 'Scott', lastName: 'Allen'}
+      ];
+
+      const expected = [
+        {value: 'austin-smith', text: 'Austin Smith'},
+        {value: 'scott-allen', text: 'Scott Allen'}
+      ];
+
+      expect(authorsFormattedForDropDown(authors)).toEqual(expected);
+    });
+  });
+});
