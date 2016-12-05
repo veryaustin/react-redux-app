42553245f331d7b58075f7da4d948ab04884bf41
Austin Lauritsen <austin@veryaustin.com>
2016-12-05 13:14:01 -0600

    Creating a new course functionality

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,6 +5,14 @@ export function loadCoursesSuccess(courses) {
   return {type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function updateCourseSuccess(course) {
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
+export function createCourseSuccess(course) {
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
 export function loadCourses() {
  return function(dispatch) {
    return courseApi.getAllCourses().then(courses => {
@@ -13,4 +21,15 @@ export function loadCourses() {
     throw(error);
    });
  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
+    }).catch(error => {
+      throw (error);
+    });
+  };
 }
