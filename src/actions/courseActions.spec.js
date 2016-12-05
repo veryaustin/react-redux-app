83c47787b4010afe84ddb000664eee3a8c444233
Austin Lauritsen <austin@veryaustin.com>
2016-12-05 17:55:08 -0600

    Test thunks and Async actions

--- src/actions/courseActions.spec.js
+++ src/actions/courseActions.spec.js
@@ -1,6 +1,9 @@
 import expect from 'expect';
 import * as courseActions from './courseActions';
 import * as types from './actionTypes';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
 
 describe('Course Actions', () => {
   describe('createCourseSuccess', () => {
@@ -15,5 +18,29 @@ describe('Course Actions', () => {
 
       expect(action).toEqual(expectedAction);
     });
+  });
+});
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async Actions', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('should create BEGIN_AJAX_CALL and LOAD_COURSES_SUCCESS when loading courses', (done) => {
+    const expectedActions = [
+      {type: types.BEGIN_AJAX_CALL},
+      {type: types.LOAD_COURSES_SUCCESS, body: {courses: [{id: 'test-code', title: 'Test Codes'}]}}
+    ];
+
+    const store = mockStore({courses: []}, expectedActions);
+    store.dispatch(courseActions.loadCourses()).then(() => {
+      const actions = store.getActions();
+      expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+      expect(actions[1].type).toEqual(types.LOAD_COURSES_SUCCESS);
+      done();
+    })
   });
 });
